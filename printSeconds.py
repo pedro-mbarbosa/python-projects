18c8f4a3d553feb91c3c31da7443c249f6c4132e
pedro-mbarbosa <163596225+pedro-mbarbosa@users.noreply.github.com>
2025-10-08 16:44:13 -0400

    Update printSeconds.py

--- printSeconds.py
+++ printSeconds.py
@@ -1,5 +1,5 @@
 def print_seconds(hours, minutes, seconds):
-   print(hours*3600+minutes*60+seconds)
+   print(hours * 3600 + minutes * 60 + seconds)
 
 print_seconds(1,2,3)
-#output will print to the screen
+
