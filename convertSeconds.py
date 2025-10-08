4371d3405367a094f602b5492826cb373b9d1d4f
pedro-mbarbosa <163596225+pedro-mbarbosa@users.noreply.github.com>
2025-10-08 16:43:15 -0400

    Update convertSeconds.py

--- convertSeconds.py
+++ convertSeconds.py
@@ -1,7 +1,8 @@
 def convert_seconds(seconds):
-    hours = seconds //3600 #double slash // return as int not double 5//2 = 2 instead of 2.5
+    hours = seconds //3600 #double slash "//" returns as an integer not a double, for example : 5//2 = 2, instead of 2.5
     minutes = (seconds % 3600) // 60
     seconds = seconds % 60
     print(hours, minutes, seconds)
+
 
 convert_seconds(10000)
